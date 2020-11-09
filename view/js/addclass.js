64d184f94e318214813e2949481adf81db3a2022
hhhh <hhhh@gmail.com>
2020-11-08 18:03:12 -0600

    add Classes

--- view/js/addclass.js
+++ view/js/addclass.js
@@ -18,8 +18,8 @@ document.getElementById("add-student").addEventListener("click", function() {
             <option>Sabado</option>\
             <option>Domingo</option>\
         </select></td>\
-        <td><input type='time'></td>\
-        <td><input type='time'></td>\
+        <td><input name='Inicio' type='time' value='22:53'></td>\
+        <td><input name='Cierre' type='time' value='22:53'></td>\
     </tr>\
     ";
 })
@@ -28,4 +28,49 @@ function RemoveClass(del) {
     let row = document.getElementById(del);
     let padre = row.parentNode;
     padre.removeChild(row);
+}
+
+
+
+
+document.getElementById("guardar").addEventListener("click", function() {
+    let obj = {
+        "alumnosMax": document.getElementById("alumnos").value,
+        "codigoBaileID": document.getElementById("Tipo").value,
+        "horarios": GenerateHorario()
+    }
+
+    fetch("https://localhost:5001/api/clase", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        })
+        .then(response => response.json())
+        .catch(error => console.log("error"))
+        .then(json => {
+            //location.href = "./Login.html"
+        });
+
+})
+
+
+function GenerateHorario() {
+    let newshorarios = new Array();
+    console.log("inicio");
+    let horariosInicio = document.getElementsByName("Inicio");
+    let horariosCierre = document.getElementsByName("Cierre");
+    let row = 0;
+
+    horariosInicio.forEach(horario => (
+        newshorarios.push({
+            Apertura: "2019-07-26T" + horario.value,
+            Cierre: "2019-07-26T" + horariosCierre[row].value,
+            Dia: "Miercoles"
+        })
+    ));
+
+    newshorarios.forEach(x => console.log(x));
+    return newshorarios;
 }
