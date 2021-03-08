fd373f539e43f1cce70793da9e0fd2cf64620557
Carlos Lopez <60913078+cardanxd@users.noreply.github.com>
2021-03-08 07:01:30 -0600

    commit

--- src/routes/Student/InscripcionController.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/inscripcion', (req, res) => {
-    res.render('links/Inscripcion');
-})
-
-module.exports = router;
